405fea040f5cb619fbc284e0532747b294b84747
Oz-NoXIII <72974137+Oz-NoXIII@users.noreply.github.com>
2020-12-02 05:30:12 +0100

    ajout des vérifications sur le dictionnaire
    -isarelation
    -len_is_cst
    -arelists
    -havesametype

--- fct_utile.py
+++ fct_utile.py
@@ -1,6 +1,7 @@
 class Relation:
     def __init__(self, nom, dico):
         self.nom = nom
+        isarelation(dico)
         self.dico = dico
 
 
@@ -27,3 +28,60 @@ def removeduplicate(args):
         if args[i] not in new_args:
             new_args.append(args[i])
     return new_args
+
+
+def isarelation(dico):
+    """Fonction qui vérifie si toutes les clés pointent vers des listes de même taille contenant des éléments de même type
+
+        Lève une erreur sinon"""
+    try:
+        len_is_cst(dico)
+        for key in dico:
+            havesametype(key)
+    except ErrorKey:
+        print("Ce n'est pas une relation acceptable.")
+
+
+def len_is_cst(dico):
+    """Fonction qui vérifie si toutes les clés pointent vers des listes de même taille
+
+    Lève une erreur sinon"""
+    try:
+        arelists(dico)
+        premierPassage = True
+        for argument in dico:
+            if premierPassage:
+                a = len(argument)
+                premierPassage = False
+            elif len(argument) != a:
+                raise ErrorKey()
+    except ErrorKey:
+        print("Les tailles des listes ne sont pas constantes.")
+
+
+def arelists(dico):
+    """Fonction qui vérifie si toutes les clés pointent vers des listes
+
+       Lève une erreur sinon"""
+    try:
+        for argument in dico:
+            if type(argument) != list:
+                raise ErrorKey()
+    except ErrorKey:
+        print(f"La clé {argument} ne pointe pas vers une liste.")
+
+
+def havesametype(args):
+    """Fonction qui vérifie si toutes les éléments de la liste sont du même type
+
+        Lève une erreur sinon"""
+    try:
+        premierPassage = True
+        for argument in args:
+            if premierPassage:
+                a = type(argument)
+                premierPassage = False
+            elif type(argument) != a:
+                raise ErrorKey()
+    except ErrorKey:
+        print(f"Les éléments de la liste ne sont pas du même type.")
