0082237b08f3733cd7c14aad736df95d782a6caf
Oz-NoXIII <72974137+Oz-NoXIII@users.noreply.github.com>
2020-12-04 06:52:26 +0100

    Update des fichiers
    corr des bugs mineurs et des erreurs du au non-testage des fonctions
    ajout des différents tests unitaires
    ajout de la documentation des fonctions

--- Test_fct_utile.py
+++ Test_fct_utile.py
@@ -1,18 +1,108 @@
 import unittest
-from fct_utile import Relation, argsinrel, removeduplicate, ErrorKey
+from fct_utile import Relation, argsinrel, removeduplicate, ErrorKey, havesametype, ErrorType, arelists, len_is_cst, \
+    isarelation
 
 
 class FctUTest(unittest.TestCase):
-    """Test case utilisé pour tester les fonctions et classe du module fct_utile"""
+    """Test case utilisé pour tester les fonctions et classe du module fct_utile
 
-    def test_Relation(self):
-        pass # TODO restreindre dico à un dictionnaire de listes de taille constante.
+    Affiche normalement ces avertissements dans la console:
 
-    def test_argsinrel(self):
-        pass
+    La clé b ne pointe pas vers une liste.
+
+    Il y a au moins deux types différents dans la liste dont <class 'int'> et <class 'str'>.
+
+    Format Incompatible: <class 'int'> différent de <class 'str'>
+
+    Format Incompatible: <class 'int'> différent de <class 'float'>
+
+    Les tailles des listes ne sont pas constantes.
+
+    Format Incompatible: <class 'int'> différent de <class 'str'>
+
+    La clé a pointe vers une liste vide.
+
+    Les tailles des listes ne sont pas constantes."""
+
+    def test_havesametype(self):
+        """Teste le fonctionnement de la fonction havesametype"""
+        args = "a"
+        with self.assertRaises(ErrorType):
+            havesametype(args)
+        args = ["a", 1, "b"]
+        self.assertFalse(havesametype(args))
+        args = ["a", 1]
+        self.assertFalse(havesametype(args))
+        args = [1.0, 1]
+        self.assertFalse(havesametype(args))
+        args = ["a", "b"]
+        self.assertTrue(havesametype(args))
+
+    def test_arelists(self):
+        """Teste le fonctionnement de la fonction arelists"""
+        dico = "a"
+        with self.assertRaises(ErrorType):
+            arelists(dico)
+        dico = {"a": [], "b": 2}
+        self.assertFalse(arelists(dico))
+        dico = {"a": [], "b": []}
+        self.assertTrue(arelists(dico))
+
+    def test_len_is_cst(self):
+        """Teste le fonctionnement de la fonction len_is_cst"""
+
+        dico = "a"
+        with self.assertRaises(ErrorType):
+            arelists(dico)
+        dico = {"a": [], "b": []}
+        self.assertFalse(len_is_cst(dico))
+        dico = {"a": [1], "b": []}
+        self.assertFalse(len_is_cst(dico))
+        dico = {"a": [1], "b": [2]}
+        self.assertTrue(len_is_cst(dico))
+
+    def test_isarelation(self):
+        """Teste le fonctionnement de la fonction isarelation"""
+
+        dico = "a"
+        with self.assertRaises(ErrorType):
+            arelists(dico)
+        dico = {"a": ["a"], "b": ["b", "c"]}
+        self.assertFalse(len_is_cst(dico))
+        self.assertTrue((havesametype(dico["b"])))
+        self.assertTrue(havesametype(dico["a"]))
+        dico = {"a": ["a", 1], "b": ["b", "c"]}
+        self.assertTrue(len_is_cst(dico))
+        self.assertTrue((havesametype(dico["b"])))
+        self.assertFalse(havesametype(dico["a"]))
+        dico = {"a": [1, 2], "b": [1.1, 2.2]}
+        self.assertTrue(len_is_cst(dico))
+        self.assertTrue((havesametype(dico["a"])))
+        self.assertTrue((havesametype(dico["b"])))
 
     def test_removeduplicate(self):
-        pass
+        """Teste le fonctionnement de la fonction removeduplicate"""
+
+        args = "a"
+        with self.assertRaises(ErrorType):
+            removeduplicate(args)
+        args = ["a"]
+        self.assertTrue(args == removeduplicate(args))
+        self.assertTrue(len(args) >= len(removeduplicate(args)))
+        args = ["a", "a"]
+        self.assertFalse(args == removeduplicate(args))
+        self.assertTrue(len(args) >= len(removeduplicate(args)))
+
+    def test_argsinrel(self):
+        """Teste le fonctionnement de la fonction argsinrel"""
+
+        rel = {}
+        args = "a"
+        with self.assertRaises(ErrorType):
+            argsinrel(rel, args)
+        rel = Relation("rel", {"a": [1], "b": [2]})
+        args = ["a"]
+        self.assertTrue(argsinrel(rel, args))
 
 
 if __name__ == '__main__':
--- fct_utile.py
+++ fct_utile.py
@@ -10,19 +10,33 @@ class ErrorKey(Exception):
     pass
 
 
-def argsinrel(relation, args):
-    """Fonction qui vérifie si tous les arguments dans args font partie de la relation rel.
+class ErrorType(Exception):
+    """Exception qui signale une erreur au niveau des types"""
+    pass
 
-    Léve une erreur sinon."""
+
+def argsinrel(relation, args):
+    """Fonction qui vérifie si tous les arguments dans args font partie de la relation rel et retourne True.
+
+    Léve une erreur et retourne False sinon."""
+
+    if type(relation) != Relation or type(args) != list:
+        raise ErrorType()
     try:
         for arg in args:
-            if arg not in relation:
+            if arg not in relation.dico:
                 raise ErrorKey()
+        return True
     except ErrorKey:
         print(f"L'argument {arg} n'appartient pas à la relation {relation}.")
+        return False
 
 
 def removeduplicate(args):
+    """Fonction qui supprime les doublons dans la liste"""
+
+    if type(args) != list:
+        raise ErrorType()
     new_args = []
     for i in range(len(args)):
         if args[i] not in new_args:
@@ -31,50 +45,67 @@ def removeduplicate(args):
 
 
 def isarelation(dico):
-    """Fonction qui vérifie si toutes les clés pointent vers des listes de même taille contenant des éléments de même type
+    """Fonction qui vérifie si toutes les clés pointent vers des listes non-vides de même taille contenant des éléments
+    de même type"""
 
-        Lève une erreur sinon"""
-    try:
-        len_is_cst(dico)
-        for key in dico:
-            havesametype(key)
-    except ErrorKey:
-        print("Ce n'est pas une relation acceptable.")
+    if type(dico) != dict:
+        raise ErrorType()
+    len_is_cst(dico)
+    for key in dico:
+        havesametype(dico[key])
 
 
 def len_is_cst(dico):
-    """Fonction qui vérifie si toutes les clés pointent vers des listes de même taille
+    """Fonction qui vérifie si toutes les clés pointent vers des listes non-vides de même taille et retourne True.
 
-    Lève une erreur sinon"""
+    Lève une erreur et retourne False sinon."""
+
+    if type(dico) != dict:
+        raise ErrorType()
     try:
         arelists(dico)
         premierPassage = True
         for argument in dico:
             if premierPassage:
-                a = len(argument)
+                a = len(dico[argument])
                 premierPassage = False
-            elif len(argument) != a:
+                if a == 0:
+                    raise ErrorKey()
+            elif len(dico[argument]) != a:
                 raise ErrorKey()
+        return True
     except ErrorKey:
-        print("Les tailles des listes ne sont pas constantes.")
+        if a == 0:
+            print(f"La clé {argument} pointe vers une liste vide.")
+        else:
+            print("Les tailles des listes ne sont pas constantes.")
+        return False
 
 
 def arelists(dico):
-    """Fonction qui vérifie si toutes les clés pointent vers des listes
+    """Fonction qui vérifie si toutes les clés pointent vers des listes et retourne True.
 
-       Lève une erreur sinon"""
+    Lève une erreur et retourne False sinon."""
+
+    if type(dico) != dict:
+        raise ErrorType()
     try:
         for argument in dico:
-            if type(argument) != list:
+            if type(dico[argument]) != list:
                 raise ErrorKey()
+        return True
     except ErrorKey:
         print(f"La clé {argument} ne pointe pas vers une liste.")
+        return False
 
 
 def havesametype(args):
-    """Fonction qui vérifie si toutes les éléments de la liste sont du même type
+    """Fonction qui vérifie si toutes les éléments de la liste sont du même type et retourne True.
 
-        Lève une erreur sinon"""
+    Lève une erreur et retourne False sinon."""
+
+    if type(args) != list:
+        raise ErrorType()
     try:
         premierPassage = True
         for argument in args:
@@ -82,6 +113,11 @@ def havesametype(args):
                 a = type(argument)
                 premierPassage = False
             elif type(argument) != a:
-                raise ErrorKey()
-    except ErrorKey:
-        print(f"Les éléments de la liste ne sont pas du même type.")
+                raise ErrorType()
+        return True
+    except ErrorType:
+        if len(args) > 2:
+            print(f"Il y a au moins deux types différents dans la liste dont {type(argument)} et {a}.")
+        else:
+            print(f"Format Incompatible: {type(argument)} différent de {a}")
+        return False
